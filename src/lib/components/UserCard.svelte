28819125f61657eab67bd30451b0993046cbe8bf
Mark Suckerberg <mark@suckerberg.gay>
2025-10-14 22:52:45 -0500

    User card decoration

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import type { LimitedUserInfo } from '$lib/userinfo';
+	import Medal from '@lucide/svelte/icons/medal';
 	import { Avatar } from '@skeletonlabs/skeleton-svelte';
 
 	let { info }: { info: LimitedUserInfo } = $props();
+
+	const countClass =
+		info.placed > 100000
+			? 'text-yellow-300'
+			: info.placed > 50000
+				? 'text-gray-300'
+				: info.placed > 10000
+					? 'text-amber-700'
+					: '';
 </script>
 
 <Avatar
@@ -11,8 +21,13 @@
 	border={info.mod ? 'border-2 border-amber-400' : ''}
 />
 <div>
-	<h3 class="font-bold">{info.name}</h3>
-	<p>
+	<h3 class="font-bold">
+		{info.name}
+		{#if info.placed > 10000}
+			<Medal class={`${countClass} inline`} />
+		{/if}
+	</h3>
+	<p class={countClass}>
 		{info.placed.toLocaleString()} pixels placed
 	</p>
 </div>
